64299bec86ee4bd362a7b15ff9ed095c14e635b3
allnews500731 <allnews500731@gmail.com>
2017-03-10 17:25:25 +0800

    Example04: 加入錯誤後5斷程式機制

--- Example04/Program.cs
+++ Example04/Program.cs
@@ -18,11 +18,23 @@ namespace Example04
             }
             catch (Exception e)
             {
-                Console.WriteLine();
+                Console.WriteLine("第一個數字輸入錯誤");
+                Console.WriteLine("錯誤訊息:\n" + e.ToString());
+                return;
             }
 
+            int number2 = 0;
             Console.WriteLine("請輸入第二個數字");
-            int number2 = int.Parse(Console.ReadLine());
+            try
+            {
+                number2 = int.Parse(Console.ReadLine());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("第二個數字輸入錯誤");
+                Console.WriteLine("錯誤訊息:\n" + e.ToString());
+                return;
+            }
 
             Console.WriteLine("{0}+{1}={2}", number1, number2, number1 + number2);
         }
